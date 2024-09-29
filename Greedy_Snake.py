6ca4028aade49469040adf02970a9b71b03b57fb
PiesAvalon <167538020+PiesAvalon@users.noreply.github.com>
2024-09-29 14:57:57 +0800

    Greedy_Snake.py1.2.2
    优化了部分代码
    修正了边界问题的bug

--- Greedy_Snake.py
+++ Greedy_Snake.py
@@ -1,27 +1,47 @@
 '''
-Greedy_Snake.py 1.2版本
-***增加了难度选择
+Greedy_Snake.py 1.2.2版本
 ***优化了部分代码
-***修正了蛇向前时按下相反键会撞到自己的bug
+***修正了边界问题的bug
 '''
 from random import randrange
-from turtle import *
-from time import sleep
-from gamebase import *
+import gamebase
+import turtle
 
-X = randrange(-15,15)*10
-Y = randrange(-15,15)*10
-moveX = 10
-moveY = 0
-moveX2 = -10
-moveY2 = 0
-Snake = [[0,0],[10,0],[20,0],[30,0],[40,0],[50,0]]
-Snake2 = [[0,0],[-10,0],[-20,0],[-30,0],[-40,0],[-50,0]]
-n = 0
-m = 0
-crush = 0
-inp = 0
-speed = 100
+def initialize():
+    turtle.title("Greedy Snake")
+    turtle.bgcolor("lightblue")
+    turtle.setup(420,420,0,0)
+    turtle.listen()
+    
+    global X, Y
+    global moveX, moveY, moveX2, moveY2
+    global Snake, Snake2
+    global m, n, crush
+    X = randrange(-15,15)*10
+    Y = randrange(-15,15)*10
+    moveX = 10
+    moveY = 0
+    moveX2 = -10
+    moveY2 = 0
+    Snake = [[0,0],[10,0],[20,0],[30,0],[40,0],[50,0]]
+    Snake2 = [[0,0],[-10,0],[-20,0],[-30,0],[-40,0],[-50,0]]
+    n = 0
+    m = 0
+    crush = 0
+
+
+    #bind all keys
+    turtle.onkey(lambda: change(0,10),"w")
+    turtle.onkey(lambda: change(-10,0),"a")
+    turtle.onkey(lambda: change(0,-10),"s")
+    turtle.onkey(lambda: change(10,0),"d")
+
+    turtle.onkey(lambda: change2(0,10),"Up")
+    turtle.onkey(lambda: change2(-10,0),"Left")
+    turtle.onkey(lambda: change2(0,-10),"Down")
+    turtle.onkey(lambda: change2(10,0),"Right")
+
+
 def snakecrush():
     for crush in range(len(Snake)-1):
        if Snake[-1] == Snake[crush]:
@@ -33,11 +53,11 @@ def snakecrush2():
            return True
        
 def inside():
-    if Snake[-1][0] >= 190 or Snake[-1][1] >= 200 or Snake[-1][0] <= -210 or Snake[-1][1] <= -220:
+    if Snake[-1][0] >= 190 or Snake[-1][1] >= 200 or Snake[-1][0] <= -210 or Snake[-1][1] <= -200:
         return True
     
 def inside2():
-    if Snake2[-1][0] >= 190 or Snake2[-1][1] >= 200 or Snake2[-1][0] <= -210 or Snake2[-1][1] <= -220:
+    if Snake2[-1][0] >= 190 or Snake2[-1][1] >= 200 or Snake2[-1][0] <= -210 or Snake2[-1][1] <= -200:
         return True
     
 def change(x,y):
@@ -61,51 +81,37 @@ def GameLoop():
         return
     if not (X == Snake[-1][0] and Y == Snake[-1][1]):
         Snake.pop(0)
-        clear()
+        turtle.clear()
     else:
         X = randrange(-15,15)*10
         Y = randrange(-15,15)*10
-    square(-210,-200,410,"black")
-    square(-200,-190,390,"white")
-    square(X,Y,10,"green")
+    gamebase.square(-210,-200,410,"black")
+    gamebase.square(-200,-190,390,"white")
+    gamebase.square(X,Y,10,"green")
     for n in range(len(Snake)):
-        square(Snake[n][0],Snake[n][1],10,"blue")
+        gamebase.square(Snake[n][0],Snake[n][1],10,"blue")
         n = n+1
-    ontimer(GameLoop,speed)
+    
+    if playerNum == 2:
+        Snake2.append([Snake2[-1][0] + moveX2,Snake2[-1][1] + moveY2])
+        if inside2():
+            return
+        if snakecrush2():
+            return
+        if not (X == Snake2[-1][0] and Y == Snake2[-1][1]):
+            Snake2.pop(0)
+        else:
+            X = randrange(-15,15)*10
+            Y = randrange(-15,15)*10
+        for m in range(len(Snake2)):
+            gamebase.square(Snake2[m][0],Snake2[m][1],10,"red")
+            m = m+1
+    turtle.ontimer(GameLoop,speed)
 
-def GameLoop2():
-    global X,Y
-    Snake2.append([Snake2[-1][0] + moveX2,Snake2[-1][1] + moveY2])
-    if inside2():
-        return
-    if snakecrush2():
-        return
-    if not (X == Snake2[-1][0] and Y == Snake2[-1][1]):
-        Snake2.pop(0)
-    else:
-        X = randrange(-15,15)*10
-        Y = randrange(-15,15)*10
-    for m in range(len(Snake2)):
-        square(Snake2[m][0],Snake2[m][1],10,"red")
-        m = m+1
-    ontimer(GameLoop2,speed)
 
-inp = input("单人版(W A S D)请输入'1'，双人版(I J K L)请输入'2':")
-speed = int(input("速度选择(50快 100中速 200慢):"))
-setup(420,420,0,0)
-GameLoop()
-
-if inp == '2':
-    GameLoop2()
-
-listen()
-onkey(lambda: change(0,10),"w")
-onkey(lambda: change(-10,0),"a")
-onkey(lambda: change(0,-10),"s")
-onkey(lambda: change(10,0),"d")
-
-onkey(lambda: change2(0,10),"i")
-onkey(lambda: change2(-10,0),"j")
-onkey(lambda: change2(0,-10),"k")
-onkey(lambda: change2(10,0),"l")
-done()  
+if __name__ == "__main__":
+    playerNum = int(input("单人版(W A S D)请输入'1'，双人版(上下左右)请输入'2':"))
+    speed = int(input("速度选择(50快 100中速 200慢):"))
+    initialize()
+    GameLoop()
+    turtle.done()  
